eae16acf7e6422568dfe440e629b7ff9eb184c5d
rushi3691 <rushi3691@outlook.com>
2023-05-03 16:30:01 +0530

    code

---
+++ app/main.py
@@ -0,0 +1,122 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+from flask import Flask, jsonify, request, make_response
+from heyoo import WhatsApp
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+API_TOKEN = os.environ.get("WHATSAPP_API_TOKEN")
+NUMBER_ID = os.environ.get("WHATSAPP_NUMBER_ID")
+VERIFY_TOKEN = os.environ.get('WHATSAPP_HOOK_TOKEN')
+
+
+messenger = WhatsApp(API_TOKEN,  phone_number_id=NUMBER_ID)
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/send_message", methods=['POST'])
+def send_message():
+    response = messenger.send_message(request.json['message'], request.json['to'])
+    return jsonify(
+        {
+            "data": response,
+            "status": "success",
+        }
+    ), 200
+
+@app.get("/webhook")
+def verify_token():
+    if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+        logging.info("Verified webhook")
+        response = make_response(request.args.get("hub.challenge"), 200)
+        response.mimetype = "text/plain"
+        return response
+    logging.error("Webhook Verification failed")
+    return "Invalid verification token"
+
+@app.post("/webhook")
+def hook():
+    # Handle Webhook Subscriptions
+    data = request.get_json()
+    logging.info("Received webhook data: %s", data)
+    changed_field = messenger.changed_field(data)
+    if changed_field == "messages":
+        new_message = messenger.is_message(data)
+        if new_message:
+            mobile = messenger.get_mobile(data)
+            name = messenger.get_name(data)
+            message_type = messenger.get_message_type(data)
+            logging.info(
+                f"New Message; sender:{mobile} name:{name} type:{message_type}"
+            )
+            if message_type == "text":
+                message = messenger.get_message(data)
+                name = messenger.get_name(data)
+                logging.info("Message: %s", message)
+                messenger.send_message(f"Hi {name}, nice to connect with you", mobile)
+                messenger.send_message(message, mobile)
+
+
+            elif message_type == "interactive":
+                message_response = messenger.get_interactive_response(data)
+                interactive_type = message_response.get("type")
+                message_id = message_response[interactive_type]["id"]
+                message_text = message_response[interactive_type]["title"]
+                logging.info(f"Interactive Message; {message_id}: {message_text}")
+
+            elif message_type == "location":
+                message_location = messenger.get_location(data)
+                message_latitude = message_location["latitude"]
+                message_longitude = message_location["longitude"]
+                logging.info("Location: %s, %s", message_latitude, message_longitude)
+
+            elif message_type == "image":
+                image = messenger.get_image(data)
+                image_id, mime_type = image["id"], image["mime_type"]
+                image_url = messenger.query_media_url(image_id)
+                image_filename = messenger.download_media(image_url, mime_type)
+                logging.info(f"{mobile} sent image {image_filename}")
+
+            elif message_type == "video":
+                video = messenger.get_video(data)
+                video_id, mime_type = video["id"], video["mime_type"]
+                video_url = messenger.query_media_url(video_id)
+                video_filename = messenger.download_media(video_url, mime_type)
+                logging.info(f"{mobile} sent video {video_filename}")
+
+            elif message_type == "audio":
+                audio = messenger.get_audio(data)
+                audio_id, mime_type = audio["id"], audio["mime_type"]
+                audio_url = messenger.query_media_url(audio_id)
+                audio_filename = messenger.download_media(audio_url, mime_type)
+                logging.info(f"{mobile} sent audio {audio_filename}")
+
+            elif message_type == "document":
+                file = messenger.get_document(data)
+                file_id, mime_type = file["id"], file["mime_type"]
+                file_url = messenger.query_media_url(file_id)
+                file_filename = messenger.download_media(file_url, mime_type)
+                logging.info(f"{mobile} sent file {file_filename}")
+            else:
+                logging.info(f"{mobile} sent {message_type} ")
+                logging.info(data)
+        else:
+            delivery = messenger.get_delivery(data)
+            if delivery:
+                logging.info(f"Message : {delivery}")
+            else:
+                logging.info("No new message")
+    return "OK", 200
